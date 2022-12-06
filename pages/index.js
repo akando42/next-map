cd6a91fd611ab391ea6d5ed79bc4a90541a65a86
Mike Do Consulter <hoang.do.2102@gmail.com>
2022-12-06 12:34:53 +0700

    layout updates

--- pages/index.js
+++ pages/index.js
@@ -15,35 +15,40 @@ export default class Main extends Component {
       streaming: true,
       items: [
         {
-          'image_url':'https://spykman.mikedoconsulting.com/static/f1968ff9736c1b2ac8fcb4bbeb5a67c2/7e926/spykman-world-devil_eggs_in_one_basket.webp',
-          'title':'The Inherated Empire Building Formula',
-          'date':'06.12.2022'
+          'image_url': 'https://spykman.mikedoconsulting.com/static/6eb8dac791348816c78fe0d66b23b497/af5c2/spykman-world-artOfEightLimbs.webp',
+          'title': 'Thailand - A Forever Puppet or Independent at Last',
+          'date':'25.01'
+        },
+        {
+          'image_url':'https://spykman.mikedoconsulting.com/static/d929a7561039488a6007efbce692ba1b/7e926/spykman-world-benh_vien_108_zing_9.webp',
+          'title':'Military Medicine - the Innovator of Medical Industry',
+          'date':'02.02'
+        },
+        {
+          'image_url':'https://spykman.mikedoconsulting.com/static/b2b6ff3f8fb18af1ebc07c7bd53fc37b/7e926/spykman-world-island-nation.webp',
+          'title':'An Island Nation And the Geographical Constraint That Trigger Evolution',
+          'date':'02.15'
+        },
+        {
+          'image_url':'https://spykman.mikedoconsulting.com/static/6a4e865269f3c16363aeeaecfcff2209/11d17/spykman-world-permanent.webp',
+          'title':'Richard Nixon and the Art of Manipulations',
+          'date':'02.21'
+        },
+        {
+          'image_url':'https://spykman.mikedoconsulting.com/static/db66b1561dcc701a91c8df55192a9278/fabdf/spykman-world-who_let_the_dog_out.webp',
+          'title':'Rimland Met Heartland - An Impossible Alliance',
+          'date':'02.29'
+        },
+        {
+          'image_url':'https://spykman.mikedoconsulting.com/static/e3a248653ebcebb9ebbd3e07ab738c05/64c2a/spykman-world-going-forward-to-keep-balance.webp',
+          'title':'Range the Evolution of Artilery',
+          'date':'03.13'
         },
         {
           'image_url':'https://spykman.mikedoconsulting.com/static/f1968ff9736c1b2ac8fcb4bbeb5a67c2/7e926/spykman-world-devil_eggs_in_one_basket.webp',
           'title':'The Inherated Empire Building Formula',
-          'date':'06.12.2022'
+          'date':'06.12'
         },
-        {
-          'image_url':'https://spykman.mikedoconsulting.com/static/f1968ff9736c1b2ac8fcb4bbeb5a67c2/7e926/spykman-world-devil_eggs_in_one_basket.webp',
-          'title':'The Inherated Empire Building Formula',
-          'date':'06.12.2022'
-        },
-        {
-          'image_url':'https://spykman.mikedoconsulting.com/static/f1968ff9736c1b2ac8fcb4bbeb5a67c2/7e926/spykman-world-devil_eggs_in_one_basket.webp',
-          'title':'The Inherated Empire Building Formula',
-          'date':'06.12.2022'
-        },
-        {
-          'image_url':'https://spykman.mikedoconsulting.com/static/f1968ff9736c1b2ac8fcb4bbeb5a67c2/7e926/spykman-world-devil_eggs_in_one_basket.webp',
-          'title':'The Inherated Empire Building Formula',
-          'date':'06.12.2022'
-        },
-        {
-          'image_url':'https://spykman.mikedoconsulting.com/static/f1968ff9736c1b2ac8fcb4bbeb5a67c2/7e926/spykman-world-devil_eggs_in_one_basket.webp',
-          'title':'The Inherated Empire Building Formula',
-          'date':'06.12.2022'
-        }
       ]
     }
   }
@@ -70,9 +75,9 @@ export default class Main extends Component {
             }}
             >
           </div>
-          <div className={styles.mapContainer}>
-            <Map />          
-          </div>
+          
+          <Map />          
+          
           <div className={styles.timeline}>
             { 
                 this.state.items.map(
