b2a58f4f393d65b6b1525383d7a3579bae19b430
Mike Do Consulter <hoang.do.2102@gmail.com>
2023-10-07 15:50:17 +0700

    updates Clock function

--- pages/api/clock/clock.js
+++ pages/api/clock/clock.js
@@ -6,14 +6,27 @@ const handler = async function (event, context) {
   let d = new Date()
   let hanoiZone = 'Asia/Bangkok'
   let caliZone = 'America/Los_Angeles'
+
   let hanoiTime = d.toLocaleString('en-GB', { timeZone: hanoiZone });
+  let hanoiHour = parseInt(d.toLocaleTimeString('en-GB', { timeZone: hanoiZone }));
+  
   let caliTime = d.toLocaleString('en-GB', { timeZone: caliZone });
+  let caliHour = parseInt(d.toLocaleTimeString('en-GB', { timeZone: caliZone }));
+
   let message = `Now in Vietnam is ${hanoiTime} and \n in California is ${caliTime}. Conference call is possible!!!`
   let remoteURL = "https://geogenetics.dystillvision.com/content/posts/12-06/RUEXICO.png"
 
-  if (parseInt(hanoiTime) > 8 && parseInt(hanoiTime) < 22 && parseInt(caliTime) > 8 && parseInt(caliTime) < 22){
+  // let earlyTime = 
+  // let lateTime 
+
+  console.log("TIME", caliTime, hanoiTime);
+  console.log("CALI", typeof caliHour, caliHour);
+  console.log("HANOI", typeof hanoiHour, hanoiHour);
+
+  if (hanoiHour> 8 && hanoiHour < 22 && caliHour > 8 && caliHour < 22){
     try {
       console.log(message)
+      console.log("HOUR in number", hanoiHour, caliHour)
       //////// Tweet out text only ///////////
       // await twitterClient.v2.tweet(message)
      
@@ -43,7 +56,7 @@ const handler = async function (event, context) {
     } catch(e){
       let message = "#NETLIFY clock function ERROR: "+e.message
       console.log(message)
-      await twitterClient.v2.tweet(message)
+      // await twitterClient.v2.tweet(message)
       return {
         statusCode: 400,
         message: e
@@ -51,25 +64,23 @@ const handler = async function (event, context) {
     }
   } else {
     try {
-      if (parseInt(hanoiTime) <= 8 || parseInt(hanoiTime) >= 22){
-        console.log(hanoiTime, parseInt(hanoiTime));
+      if (hanoiHour <= 8 || hanoiHour >= 22){
+        console.log(hanoiTime, hanoiHour);
         await twitterClient.v2.tweet(`Now is ${hanoiTime} in Hanoi. Citi-Zens are sleeping or sleepy`);
 
-      } else if (parseInt(caliTime) <= 8 || parseInt(caliTime) >= 22){
-        console.log(caliTime, parseInt(caliTime));
+      } else if (caliHour <= 8 || caliHour >= 22){
+        console.log(caliTime, caliHour);
         await twitterClient.v2.tweet(`Now is ${caliTime} in California. Citi-Zens are sleeping or sleepy`);
 
       } else {
         console.log("Uncaught Condition");
-        console.log("CALI", caliTime, parseInt(caliTime));
-        console.log("HANOI", hanoiTime, parseInt(hanoiTime));
-
+        console.log("CALI", caliHour);
+        console.log("HANOI", hanoiHour);
       }
     } catch(e){
       let message = "#NETLIFY clock function ERROR: "+e.message
       await twitterClient.v2.tweet(message)
-    }
-    
+    } 
   }
 };
 
